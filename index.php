cdc16c1aa3a854e9e7e1a10d39ce59a5cc42ccd7
fred-rowell <38262313+fred-rowell@users.noreply.github.com>
2018-04-11 14:39:38 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
  <?php 
-echo "<img src=CDS_Solutions  Icons_CloudCompanion.jpg></img>";
+ echo '<img src="CDS_Solutions  Icons_CloudCompanion.jpg"/>';
  echo "My Pod's IP is ".$_SERVER['SERVER_ADDR'] ;
 
  ?>
